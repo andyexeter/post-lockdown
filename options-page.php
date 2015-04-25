2170f66a0302139a84844e8268ba4fc7f8721ce5
= <palmer.andy@gmail.com>
2015-04-25 01:42:18 +0100

    Refactor

--- options-page.php
+++ options-page.php
@@ -2,14 +2,14 @@
 	<h2>Post Lockdown</h2>
 	<p>Choose which posts to disable deletion/trashing</p>
 	<form action="options.php" method="post">
-		<?php settings_fields( 'post-lockdown' ); ?>
+		<?php settings_fields( $slug ); ?>
 		<table class="form-table">
 			<tbody>
 				<?php foreach($post_types as $post_type) { ?>
 				<tr>
 					<th><?php echo $post_type['label']; ?></th>
 					<td>
-						<select name="postlockdown_locked_posts[]" multiple="multiple" style="min-width:450px;height:150px">
+						<select name="<?php echo $key; ?>[]" multiple="multiple" style="min-width:450px;height:150px">
 							<?php foreach( $post_type['posts'] as $the_post ) { ?>
 							<option value="<?php echo $the_post['ID']; ?>"<?php selected( $the_post['selected'], true ); ?>><?php echo $the_post['post_title']; ?></option>
 							<?php } ?>
--- post-lockdown.php
+++ post-lockdown.php
@@ -1,49 +1,60 @@
 <?php
 /**
  * Plugin Name: PostLockdown
- * Plugin URI: http://www.exleysmith.com
+ * Plugin URI: http://www.andypalmer.me
  * Description: Allows admins to prevent certain posts of any post type from being deleted by lower users
- * Version: 0.1.0
- * Author: Exley and Smith Ltd
- * Author URI: http://www.exleysmith.com
+ * Version: 0.4.1
+ * Author: Andy Palmer
+ * Author URI: http://www.andypalmer.me
  * License: GPL2
- * Text Domain: post-lockdown
- * Domain Path: /languages
  */
 
 PostLockdown::init();
 
 class PostLockdown {
 
+	const CAP = 'manage_options';
+	const KEY = 'postlockdown_locked_posts';
+	const SLUG = 'post-lockdown';
+
 	public static $locked_post_ids = array();
-	public static $cap = 'manage_options';
+
+	public static $caps = array( 'delete_post' => true, 'publish_pages' => true, 'publish_posts' => true );
 
 	public static function init() {
 
-		self::$locked_post_ids = array_flip( get_option( 'postlockdown_locked_posts', array() ) );
+		self::$locked_post_ids = array_flip( get_option( self::KEY, array() ) );
 
 		add_filter( 'user_has_cap', array(__CLASS__, 'filter_cap'), 10, 3 );
 
+		add_action( 'admin_init', array(__CLASS__, 'register_setting' ) );
 		add_action( 'admin_menu', array(__CLASS__, 'add_options_page') );
-		add_action( 'admin_init', array(__CLASS__, 'register_settings' ) );
-
 	}
 
+	/**
+	 * Sets the capability to false when current_user_can() has been
+	 * called on one of our self::$caps on a locked post. If the user has the
+	 * self::CAP capability we bail out early
+	 * @todo Stop users being able to change post_status to Draft / Review
+	 */
 	public static function filter_cap($allcaps, $cap, $args) {
 
-		if ( $args[0] != 'delete_post' && $args[0] != 'publish_pages' && $args[0] != 'publish_posts' ) {
-			return $allcaps;
-		}
+		$post = get_post();
 
-		if ( !empty( $allcaps[ self::$cap ] ) ) {
+		if ( !isset( self::$caps[ $args[0] ] ) || !empty( $allcaps[ self::CAP ] ) ) {
 			return $allcaps;
 		}
 
 		if ( isset( $args[2] ) ) {
 			$post_id = $args[2];
-		} else {
-			global $post;
+		} elseif( isset( $post->ID ) ) {
 			$post_id = $post->ID;
+		} else {
+			$post_id = (int)filter_input( INPUT_POST, 'post_ID', FILTER_SANITIZE_NUMBER_INT );
+		}
+
+		if( !$post_id ) {
+			return $allcaps;
 		}
 
 		if ( isset( self::$locked_post_ids[ $post_id ] ) ) {
@@ -53,12 +64,12 @@ class PostLockdown {
 		return $allcaps;
 	}
 
-	public static function add_options_page() {
-		add_options_page('post-lockdown', 'Post Lockdown', self::$cap, 'post-lockdown', array(__CLASS__, 'output_options_page') );
+	public static function register_setting() {
+		register_setting( self::SLUG, self::KEY );
 	}
 
-	public static function register_settings() {
-		register_setting( 'post-lockdown', 'postlockdown_locked_posts' );
+	public static function add_options_page() {
+		add_options_page( self::SLUG, 'Post Lockdown', self::CAP, self::SLUG, array( __CLASS__, 'output_options_page' ) );
 	}
 
 	public static function output_options_page() {
@@ -92,8 +103,9 @@ class PostLockdown {
 
 		}
 
+		$key = self::KEY;
+		$slug = self::SLUG;
+
 		include_once( __DIR__ . '/options-page.php' );
-
 	}
-
 }
