a3d66e471f7a5c90fbbd0d8768c0a1a8f8c842bd
= <palmer.andy@gmail.com>
2015-04-30 22:17:25 +0100

    Add PHP Lint and PHPCodeSniffer script

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+files=$(find . -mindepth 1 -maxdepth 1 -type f -name '*.php')
+
+echo "Running PHP Linter..."
+echo ""
+for i in $files; do
+	php -l $i
+done
+echo ""
+echo "Running PHPCodeSniffer..."
+for i in $files; do
+	phpcs --standard="WordPress-Extra" $i
+done
