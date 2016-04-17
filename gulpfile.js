f8b08f62ea5e7af3ba55cebc846cd6739552b9c6
andyexeter <palmer.andy@gmail.com>
2016-04-18 00:23:17 +0100

    Move from Grunt to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,102 @@
-var gulp = require('gulp'),
-	jshint = require('gulp-jshint'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-	sourcemaps = require('gulp-sourcemaps'),
-	cssmin = require('gulp-minify-css'),
-	imagemin = require('gulp-imagemin');
+(function() {
+	'use strict';
 
-var paths = {
-	js: {
-		src: 'src/assets/js/',
-		dest: 'build/assets/js/'
-	},
-	css: {
-		src: 'src/assets/sass/',
-		dest: 'build/assets/css/'
-	},
-	img: {
-		src: 'src/assets/img/',
-		dest: 'build/assets/img/'
+	var gulp = require( 'gulp' ),
+		//debug = require( 'gulp-debug' ),
+		jshint = require( 'gulp-jshint' ),
+		//watch = require( 'gulp-watch' ),
+		concat = require( 'gulp-concat' ),
+		uglify = require( 'gulp-uglify' ),
+		rename = require( 'gulp-rename' ),
+		sourcemaps = require( 'gulp-sourcemaps' ),
+		sass = require( 'gulp-sass' ),
+		cssmin = require( 'gulp-minify-css' );
+
+	var options = {
+		jsBuildFile: 'postlockdown',
+		cssBuildFile: 'postlockdown'
+	};
+
+	var paths = {
+		assets: {
+			src: 'src/assets/',
+			build: 'view/assets/'
+		}
+	};
+
+	addPaths( 'js' );
+	addPaths( 'css', 'sass' );
+
+	function addPaths( dest, src ) {
+		if ( arguments.length === 1 ) {
+			src = dest;
+		}
+
+		paths[ dest ] = {
+			get src() {
+				return paths.assets.src + src + '/';
+			},
+			get dest() {
+				return paths.assets.build + dest + '/';
+			}
+		};
 	}
-};
 
+	gulp.task( 'default', [ 'jshint-gulpfile', 'js', 'css' ], function() {
+	} );
 
-gulp.task( 'default', [ 'js', 'css', 'img' ], function() {} );
+	gulp.task( 'jshint-gulpfile', function() {
+		return gulp.src( 'gulpfile.js' )
+				   .pipe( jshint() )
+				   .pipe( jshint.reporter( 'default' ) )
+				   .pipe( jshint.reporter( 'fail' ) );
+	} );
 
-gulp.task( 'js', function() {
-	return gulp.src( paths.js.src + '*.js' )
-		// Lint all matching files with jshint
-		.pipe( jshint() )
-		// Init source mapping plugin
-		.pipe( sourcemaps.init() )
-		// Concatenate the stream
-		.pipe( concat( 'main.js' ) )
-		// Write main.js to the js destination path
-		.pipe( gulp.dest( paths.js.dest ) )
-		// Rename the stream to main.min.js
-		.pipe( rename( 'main.min.js' ) )
-		// Uglify the stream
-		.pipe( uglify() )
-		// Write the source map files
-		.pipe( sourcemaps.write() )
-		// Write main.min.js to the js destination path
-		.pipe( gulp.dest( paths.js.dest ) );
-} );
+	gulp.task( 'js', function() {
+		return gulp
+			.src( paths.js.src + '*.js' )
+			.pipe( jshint() )
+			.pipe( jshint.reporter( 'default' ) )
+			.pipe( jshint.reporter( 'fail' ) )
+			.pipe( sourcemaps.init() )
+			// Write the files to the destination path.
+			.pipe( concat( options.jsBuildFile + '.js' ) )
+			.pipe( gulp.dest( paths.js.dest ) )
+			.pipe( uglify() )
+			.pipe( rename( function( path ) {
+				path.extname = '.min.js';
+			} ) )
+			// Write the minified files to the destination path.
+			.pipe( gulp.dest( paths.js.dest ) )
+			.pipe( sourcemaps.write( '.', {
+				sourceRoot: paths.js.src
+			} ) )
+			// Write the source maps to the destination path.
+			.pipe( gulp.dest( paths.js.dest ) );
+	} );
 
-gulp.task( 'css', function() {
-	return gulp.src( paths.css.src + '*.sass' )
-		// Init source mapping plugin
-		.pipe( sourcemaps.init() )
-		// Concatenate the stream
-		.pipe( concat( 'style.css' ) )
-		// Write style.js to the css destination path
-		.pipe( gulp.dest( paths.css.dest ) )
-		// Rename the steam to style.min.css
-		.pipe( rename( 'style.min.css' ) )
-		// Minify the stream
-		.pipe( cssmin() )
-		// Write the source maps
-		.pipe( sourcemaps.write() )
-		// Write style.min.css to the css destination path
-		.pipe( gulp.dest( paths.css.dest ) );
-} );
+	gulp.task( 'css', function() {
+		return gulp
+			.src( paths.css.src + '*.scss' )
+			.pipe( sourcemaps.init() )
+			.pipe( sass( {
+				outputStyle: 'expanded'
+			} ).on( 'error', sass.logError ) )
+			.pipe( rename( options.cssBuildFile + '.css' ) )
+			// Write style.css to the css destination path.
+			.pipe( gulp.dest( paths.css.dest ) )
+			.pipe( rename( options.cssBuildFile + '.min.css' ) )
+			.pipe( cssmin() )
+			.pipe( sourcemaps.write( '.', {
+				sourceRoot: paths.css.src
+			} ) )
+			// Write style.min.css to the css destination path.
+			.pipe( gulp.dest( paths.css.dest ) );
+	} );
 
-gulp.task( 'img', function() {
-	return gulp.src( paths.img.src + '*.*' )
-		// Minify all matching images
-		.pipe( imagemin() )
-		// Write the minified images to the img destination path
-		.pipe( gulp.dest( paths.img.dest ) );
-} );
-
-gulp.task( 'watch', function() {
-	return gulp.watch();
-});
+	gulp.task( 'watch', function() {
+		gulp.watch( paths.js.src + '*.js', [ 'js' ] );
+		gulp.watch( paths.css.src + '*.scss', [ 'css' ] );
+		gulp.watch( paths.css.img + '*.scss', [ 'img' ] );
+	} );
+})();
