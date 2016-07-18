d9ac7e5e83bd12a753cc52e8412d44255cfe8565
andyexeter <palmer.andy@gmail.com>
2016-07-18 12:33:54 +0100

    Add explicity dom ready call

--- src/assets/js/postlockdown.js
+++ src/assets/js/postlockdown.js
@@ -1,9 +1,8 @@
-( function( $ ) {
+(function( $ ) {
 	'use strict';
 
-	$( function() {
+	$( document ).on( 'ready', function() {
 		$( '.pl-posts-container' ).each( function() {
-
 			var selectedKey = $( this ).find( '.pl-posts-selected .pl-multiselect' ).data( 'key' ),
 				inputName = $( this ).find( '.pl-posts-selected .pl-multiselect' ).data( 'input_name' );
 
@@ -13,10 +12,10 @@
 				inputSearch: $( this ).find( '.pl-autocomplete' ),
 				ulAvailable: $( this ).find( '.pl-posts-available .pl-multiselect' ),
 				ulSelected: $( this ).find( '.pl-posts-selected .pl-multiselect' ),
-				selected: window.postlockdown[ selectedKey ] || [ ],
+				selected: window.postlockdown[ selectedKey ] || [],
 				spinner: $( this ).find( '.spinner' )
 			} );
 		} );
 
 	} );
-} )( jQuery );
+})( jQuery );
