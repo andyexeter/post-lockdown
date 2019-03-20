8687065a4affdb35778bec6a99d82adb4f9eff9f
andyexeter <palmer.andy@gmail.com>
2019-03-20 13:44:13 +0000

    Format with prettier. Remove sourcemaps

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,141 +1,136 @@
-module.exports = function( grunt ) {
-	'use strict';
+module.exports = function (grunt) {
+    'use strict';
 
-	require( 'load-grunt-tasks' )( grunt );
+    require('load-grunt-tasks')(grunt);
 
-	grunt.util.linefeed = '\n';
-	grunt.option( 'stack', true );
+    grunt.util.linefeed = '\n';
+    grunt.option('stack', true);
 
-	grunt.initConfig( {
-		pkg: grunt.file.readJSON( 'package.json' ),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-		paths: {
-			src: {
-				js: 'src/assets/js',
-				css: 'src/assets/sass'
-			},
-			build: {
-				js: 'view/assets/js',
-				css: 'view/assets/css'
-			}
-		},
+        paths: {
+            src: {
+                js: 'src/assets/js',
+                css: 'src/assets/sass'
+            },
+            build: {
+                js: 'view/assets/js',
+                css: 'view/assets/css'
+            }
+        },
 
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
-			src: {
-				src: '<%= paths.src.js %>/*.js'
-			},
-			build: {
-				src: [ '<%= paths.build.js %>/*.js', '!<%= paths.build.js %>/*.min.js' ]
-			},
-			grunt: {
-				options: {
-					node: true
-				},
-				src: 'Gruntfile.js'
-			}
-		},
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            src: {
+                src: '<%= paths.src.js %>/*.js'
+            },
+            build: {
+                src: ['<%= paths.build.js %>/*.js', '!<%= paths.build.js %>/*.min.js']
+            },
+            grunt: {
+                options: {
+                    node: true
+                },
+                src: 'Gruntfile.js'
+            }
+        },
 
-		clean: {
-			buildJs: [ '<%= paths.build.js %>' ],
-			buildCss: [ '<%= paths.build.css %>' ]
-		},
+        clean: {
+            buildJs: ['<%= paths.build.js %>'],
+            buildCss: ['<%= paths.build.css %>']
+        },
 
-		sasslint: {
-			options: {},
-			target: [ '<%= paths.src.css %>/*.scss' ]
-		},
+        sasslint: {
+            options: {},
+            target: ['<%= paths.src.css %>/*.scss']
+        },
 
-		sass: {
-			options: {
-				sourceMap: true,
-				outputStyle: 'expanded',
+        sass: {
+            options: {
+                outputStyle: 'expanded',
                 implementation: require('node-sass')
-			},
-			build: {
-				src: '<%= paths.src.css %>/postlockdown.scss',
-				dest: '<%= paths.build.css %>/postlockdown.css'
-			}
-		},
+            },
+            build: {
+                src: '<%= paths.src.css %>/postlockdown.scss',
+                dest: '<%= paths.build.css %>/postlockdown.css'
+            }
+        },
 
-		cssmin: {
-			options: {
-				sourceMap: true,
-				report: 'gzip'
-			},
-			build: {
-				src: '<%= paths.build.css %>/postlockdown.css',
-				dest: '<%= paths.build.css %>/postlockdown.min.css'
-			}
-		},
+        cssmin: {
+            options: {
+                report: 'gzip'
+            },
+            build: {
+                src: '<%= paths.build.css %>/postlockdown.css',
+                dest: '<%= paths.build.css %>/postlockdown.min.css'
+            }
+        },
 
-		concat: {
-			options: {
-				sourceMap: true
-			},
-			build: {
-				src: '<%= paths.src.js %>/*.js',
-				dest: '<%= paths.build.js %>/postlockdown.js'
-			}
-		},
+        concat: {
+            options: {},
+            build: {
+                src: '<%= paths.src.js %>/*.js',
+                dest: '<%= paths.build.js %>/postlockdown.js'
+            }
+        },
 
-		uglify: {
-			options: {
-				sourceMap: true,
-				report: 'gzip'
-			},
-			build: {
-				files: [ {
-					expand: true,
-					cwd: '<%= paths.build.js %>',
-					src: '*.js',
-					dest: '<%= paths.build.js %>',
-					ext: '.min.js',
-					extDot: 'last'
-				} ]
-			}
-		},
+        uglify: {
+            options: {
+                report: 'gzip'
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= paths.build.js %>',
+                    src: '*.js',
+                    dest: '<%= paths.build.js %>',
+                    ext: '.min.js',
+                    extDot: 'last'
+                }]
+            }
+        },
 
-		watch: {
-			options: { livereload: true, spawn: false },
-			sass: {
-				files: [ '<%= paths.src.css %>/*.scss' ],
-				tasks: [ 'buildCss' ]
-			},
-			js: {
-				files: [ '<%= paths.src.js %>/*.js' ],
-				tasks: [ 'buildJs' ]
-			},
-			grunt: {
-				options: { livereload: false },
-				files: [ 'Gruntfile.js' ],
-				tasks: [ 'jshint:grunt' ]
-			},
-			livereload: {
-				files: [ '<%= paths.build.css %>/postlockdown.css' ]
-			}
-		}
+        watch: {
+            options: {livereload: true, spawn: false},
+            sass: {
+                files: ['<%= paths.src.css %>/*.scss'],
+                tasks: ['buildCss']
+            },
+            js: {
+                files: ['<%= paths.src.js %>/*.js'],
+                tasks: ['buildJs']
+            },
+            grunt: {
+                options: {livereload: false},
+                files: ['Gruntfile.js'],
+                tasks: ['jshint:grunt']
+            },
+            livereload: {
+                files: ['<%= paths.build.css %>/postlockdown.css']
+            }
+        }
 
-	} );
+    });
 
-	grunt.registerTask( 'buildJs', [
-		'jshint:src',
-		'clean:buildJs',
-		'concat:build',
-		'uglify:build'
-	] );
+    grunt.registerTask('buildJs', [
+        'jshint:src',
+        'clean:buildJs',
+        'concat:build',
+        'uglify:build'
+    ]);
 
-	grunt.registerTask( 'buildCss', [
-		'clean:buildCss',
-		'sass:build',
-		'cssmin:build'
-	] );
+    grunt.registerTask('buildCss', [
+        'clean:buildCss',
+        'sass:build',
+        'cssmin:build'
+    ]);
 
-	grunt.registerTask( 'default', [
-		'jshint:grunt',
-		'buildJs',
-		'buildCss'
-	] );
+    grunt.registerTask('default', [
+        'jshint:grunt',
+        'buildJs',
+        'buildCss'
+    ]);
 };
