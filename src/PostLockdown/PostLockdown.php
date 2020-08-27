051a5ff2332107d02162ef245a50769846152c23
Joshua Nelson <josh@joshuadnelson.com>
2020-08-27 11:06:29 +0100

    add cache arguments to get_posts for improved query performance

--- src/PostLockdown/PostLockdown.php
+++ src/PostLockdown/PostLockdown.php
@@ -183,8 +183,12 @@ class PostLockdown
     public function get_posts($args = [])
     {
         $defaults = [
-            'post_type'   => $this->get_post_types(),
-            'post_status' => ['publish', 'pending', 'draft', 'future', 'private', 'inherit'],
+            'post_type'              => $this->get_post_types(),
+            'post_status'            => ['publish', 'pending', 'draft', 'future', 'private', 'inherit'],
+            'update_post_meta_cache' => false,
+            'update_post_term_cache' => false,
+            'no_found_rows'          => true,
+            'cache_results'          => false,
         ];
 
         $args = wp_parse_args($args, $defaults);
