11ca5d381cb5fe4d9045146ef7ee6d3727796221
andyexeter <palmer.andy@gmail.com>
2025-03-26 10:23:38 +0000

    Bump version to 4.0.5

--- src/PostLockdown/PostLockdown.php
+++ src/PostLockdown/PostLockdown.php
@@ -6,7 +6,7 @@ class PostLockdown
 {
     /** Plugin key for options and the option page. */
     public const KEY     = 'postlockdown';
-    public const VERSION = '4.0.4';
+    public const VERSION = '4.0.5';
 
     /** @var array List of post IDs which cannot be edited, trashed or deleted. */
     private $locked_post_ids = [];
